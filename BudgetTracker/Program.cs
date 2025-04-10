4a3729e3ddc853e095c4283b22c0b1ed064bab14
HarperCC101 <j_turgeon@mail.harpercollege.edu>
2025-04-10 15:55:52 -0500

    Create Program.cs

---
+++ BudgetTracker/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace BudgetTrackerApp {
+    class BudgetTracker {
+        static void Main(string[] args) {
+            ConsoleUI ui = new ConsoleUI();
+            ui.Run();
+        }
+    }
+}
