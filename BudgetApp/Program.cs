b380f273f5cbb6bd19e9643d94061a8ada60f586
HarperCC101 <j_turgeon@mail.harpercollege.edu>
2025-04-15 14:52:29 -0500

    Update Program.cs

--- BudgetApp/Program.cs
+++ BudgetApp/Program.cs
@@ -1,9 +1,14 @@
+// Program.cs
 using System;
 
 namespace BudgetTrackerApp {
+    // Main class
     class BudgetTracker {
+        // Enter application
         static void Main(string[] args) {
+            // Create ConsoleUI instance (of ConsoleUI class)
             ConsoleUI ui = new ConsoleUI();
+            // Start application loop
             ui.Run();
         }
     }
